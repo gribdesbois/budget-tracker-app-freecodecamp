b9961f8e6eca014b22052cf2c1e1ce7d961eaa4a
gribdesbois <gribdesbois@gmail.com>
2022-01-22 15:11:54 +0100

    Remaining. updating state

--- vite-project/src/components/Remaining/Remaining.jsx
+++ vite-project/src/components/Remaining/Remaining.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
 
 function Remaining() {
+  const { expenses, budget } = useContext(AppContext)
+
+  const totalExpenses = expenses.reduce((total, item) => (total += item.cost), 0)
+
+  const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success'
+
   return (
-    <div className="alert alert-success">
-      <span>Remaining: 1000$</span>
+    <div className={`alert ${alertType}`}>
+      <span>Remaining: ${budget - totalExpenses}</span>
     </div>
   )
 }
