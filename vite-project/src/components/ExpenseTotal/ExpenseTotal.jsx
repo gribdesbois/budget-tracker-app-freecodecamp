5347f06ea78b15f8474f99c58bf5a7bf9d4aa1c8
gribdesbois <gribdesbois@gmail.com>
2022-01-22 15:02:11 +0100

    ExpenseTotal now updated

--- vite-project/src/components/ExpenseTotal/ExpenseTotal.jsx
+++ vite-project/src/components/ExpenseTotal/ExpenseTotal.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
 
 function ExpenseTotal() {
+  const { expenses } = useContext(AppContext)
+
+  const totalExpenses = expenses.reduce((total, item) => (total += item.cost), 0)
+
   return (
     <div className="alert alert-primary">
-      <span>Spent so far: $1000</span>
+      <span>Spent so far: ${totalExpenses}</span>
     </div>
   )
 }
